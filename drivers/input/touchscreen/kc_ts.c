1412252c617b6627a12ae4ac8d49f384dd925d91
kailua <kailua@project2by2.jp>
2026-05-15 01:02:11 +0900

    drivers: kc_ts: Use linux/uaccess.h instead asm/uaccess.h
    * error: implicit declaration of function 'copy_to_user' [-Werror,-Wimplicit-function-declaration]

--- drivers/input/touchscreen/kc_ts.c
+++ drivers/input/touchscreen/kc_ts.c
@@ -25,7 +25,7 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/input/mt.h>
 #include <linux/slab.h>
